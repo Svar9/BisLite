2c09b45a84749b78541ee71c72ddaef6182ea55e
Svar9 <tolik-pal@yandex.ru>
2020-10-28 13:46:09 +0300

    fix last commit

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,17 +1,24 @@
-$(function () {
+//= components/slick.js
+
+/* document.querySelector('.menu__btn').onclick = function() {
+    document.querySelector('.menu__btn').classList.toggle('active');
+    document.querySelector('.menu__items').classList.toggle('active');
+}; */
 
 
-    //= components/slick.js
+$('.menu__btn').on('click', function() {
+    $('.menu__btn').toggleClass('active');
+    $('.menu__items').toggleClass('active');
+});
 
-    /* document.querySelector('.menu__btn').onclick = function() {
-        document.querySelector('.menu__btn').classList.toggle('active');
-        document.querySelector('.menu__items').classList.toggle('active');
-    }; */
+document.querySelector('.menu__items.active .menu__item.menu__list').onclick = function(){
+    console.log('test');
 
 
-    $('.menu__btn').on('click', function () {
-        $('.menu__btn').toggleClass('active');
-        $('.menu__items').toggleClass('active');
-    });
-
-})
+//.menu__items.active .menu__item.menu__list
+/*     .menu__list {
+        .menu__dropdown{
+            position: static;
+            display: none;
+        } */
+};
